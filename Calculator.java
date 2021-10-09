03b02c1cce493d70bcf2f4d8bc0017c2a23b8116
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-08 20:34:43 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+package week2assignmwent;
+
+public class Calculator {
+public int add(int num1, int num2, int num3)
+{
+	int sum= num1+num2+num3;
+	return sum;
+}
+public int sub(int num1, int num2)
+{
+	int sub=num1-num2;
+	return sub;
+}
+public double mul(double num1, double num2)
+{
+	double mul=num1*num2;
+	return mul;
+}
+public float div(float num, float num2)
+{
+	float div=num/num2;
+	return div;
+	
+}
+public static void main(String[] args) {
+}
+
+}
